5e616728230eed86f5eae27dc06a142c9cb2a27a
hampus <hampus@BIANCA>
2011-05-09 11:11:45 +0200

    Signed-off-by: hampus <hampus@BIANCA>

---
+++ gitSetup/src/se/mah/soderberg/hampus/gitSetup/gitSetup.java
@@ -0,0 +1,13 @@
+package se.mah.soderberg.hampus.gitSetup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class gitSetup extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
