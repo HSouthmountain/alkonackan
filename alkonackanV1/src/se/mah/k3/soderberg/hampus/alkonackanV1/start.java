cb645db464f43e4c52a45501af1518e5370f5523
filipivansson <filipivansson@10.2.2.98>
2011-05-12 11:48:01 +0200

    Har gjort lite layout för main och options flikarna. Peace bitches!
    Signed-off-by: filipivansson <filipivansson@10.2.2.98>

--- alkonackanV1/src/se/mah/k3/soderberg/hampus/alkonackanV1/start.java
+++ alkonackanV1/src/se/mah/k3/soderberg/hampus/alkonackanV1/start.java
@@ -2,12 +2,20 @@ package se.mah.k3.soderberg.hampus.alkonackanV1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class start extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.options);
+        
+        Spinner s = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this, R.array.ManVikt, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
     }
 }
