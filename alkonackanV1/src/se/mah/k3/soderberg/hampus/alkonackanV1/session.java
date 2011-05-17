8161598495b6a488d285a08cfcc2329de9355027
hampus <hampus@BIANCA>
2011-05-17 13:38:55 +0200

    Signed-off-by: hampus <hampus@BIANCA>

--- alkonackanV1/src/se/mah/k3/soderberg/hampus/alkonackanV1/session.java
+++ alkonackanV1/src/se/mah/k3/soderberg/hampus/alkonackanV1/session.java
@@ -1,39 +1,64 @@
 package se.mah.k3.soderberg.hampus.alkonackanV1;
 
 import android.app.Activity;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SlidingDrawer;
+import android.widget.SlidingDrawer.OnDrawerCloseListener;
+import android.widget.SlidingDrawer.OnDrawerOpenListener;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class session extends Activity {
-/** Called when the activity is first created. */
-@Override
-public void onCreate(Bundle savedInstanceState) {
-super.onCreate(savedInstanceState);
-setContentView(R.layout.session);
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.session);
+        
+        SlidingDrawer offDrawer;
+        final TextView TestText;
+        
+        TestText = (TextView) this.findViewById(R.id.textViewTestText);
+        
+        // SlidingDrawer
+        offDrawer = (SlidingDrawer) this.findViewById(R.id.slidingDrawerOffDrawer);
 
-/*String[] data = new String[]{"14 mars, 2011", "15 mars, 2011", "23 mars, 2011","27 mars, 2011", "5 maj, 2011", "15 maj, 2011", "24 maj, 2011", "4 juni, 2011", "10 juni, 2011"};
-ListView lst = (ListView) findViewById( R.id.listViewSessions);
-ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.minlistitem, data );
-lst.setAdapter(adapter);
-*/
+               // Open Handler
+        offDrawer.setOnDrawerOpenListener(new OnDrawerOpenListener()
+        {
+        	@Override
+        	public void onDrawerOpened()
+        	{
+        		 //listView.setVisibility(ListView.GONE);
+        		TestText.setVisibility(TextView.GONE);
+        	}
+        });
 
-/* Spinner vikt = (Spinner) findViewById(R.id.spinner_vikt);
-ArrayAdapter adapter_vikt = ArrayAdapter.createFromResource(
-this, R.array.vikt, android.R.layout.simple_spinner_item);
-adapter_vikt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-vikt.setAdapter(adapter_vikt);
-*/
+        // Closing Handler
+        offDrawer.setOnDrawerCloseListener(new OnDrawerCloseListener()
+        {
+        	@Override
+        	public void onDrawerClosed()
+        		{
+        			// listView.setVisibility(ListView.VISIBLE);
+        			TestText.setVisibility(TextView.VISIBLE);
+        		}
+        });
 
- Spinner kanslor = (Spinner) findViewById(R.id.spinner_kanslor);
-ArrayAdapter adapter_kanslor = ArrayAdapter.createFromResource(
-this, R.array.kanslor, android.R.layout.simple_spinner_item);
-adapter_kanslor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-kanslor.setAdapter(adapter_kanslor);
-
-}
-}
+        
+        
+       /* Spinner vikt = (Spinner) findViewById(R.id.spinner_vikt);
+        ArrayAdapter adapter_vikt = ArrayAdapter.createFromResource(
+                this, R.array.vikt, android.R.layout.simple_spinner_item);
+        adapter_vikt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        vikt.setAdapter(adapter_vikt);*/
+        
+        Spinner kanslor = (Spinner) findViewById(R.id.spinner_kanslor);
+        ArrayAdapter adapter_kanslor = ArrayAdapter.createFromResource(
+                this, R.array.kanslor, android.R.layout.simple_spinner_item);
+        adapter_kanslor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        kanslor.setAdapter(adapter_kanslor);
+    }
+} 
