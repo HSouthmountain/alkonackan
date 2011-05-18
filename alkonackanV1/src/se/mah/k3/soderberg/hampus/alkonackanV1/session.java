07b23d7c10cd53d55a3cde2cbd16247fff37e508
Peter <m10k0944@student.mah.se>
2011-05-18 12:40:04 +0200

    knappar funkar

--- alkonackanV1/src/se/mah/k3/soderberg/hampus/alkonackanV1/session.java
+++ alkonackanV1/src/se/mah/k3/soderberg/hampus/alkonackanV1/session.java
@@ -11,54 +11,39 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class session extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.session);
-        
-        SlidingDrawer offDrawer;
-        final TextView TestText;
-        
-        TestText = (TextView) this.findViewById(R.id.textViewTestText);
-        
-        // SlidingDrawer
-        offDrawer = (SlidingDrawer) this.findViewById(R.id.slidingDrawerOffDrawer);
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.session);
 
-               // Open Handler
-        offDrawer.setOnDrawerOpenListener(new OnDrawerOpenListener()
-        {
-        	@Override
-        	public void onDrawerOpened()
-        	{
-        		 //listView.setVisibility(ListView.GONE);
-        		TestText.setVisibility(TextView.GONE);
-        	}
-        });
+		SlidingDrawer offDrawer;
 
-        // Closing Handler
-        offDrawer.setOnDrawerCloseListener(new OnDrawerCloseListener()
-        {
-        	@Override
-        	public void onDrawerClosed()
-        		{
-        			// listView.setVisibility(ListView.VISIBLE);
-        			TestText.setVisibility(TextView.VISIBLE);
-        		}
-        });
+		// SlidingDrawer
+		offDrawer = (SlidingDrawer) this
+				.findViewById(R.id.slidingDrawerOffDrawer);
 
-        
-        
-       /* Spinner vikt = (Spinner) findViewById(R.id.spinner_vikt);
-        ArrayAdapter adapter_vikt = ArrayAdapter.createFromResource(
-                this, R.array.vikt, android.R.layout.simple_spinner_item);
-        adapter_vikt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        vikt.setAdapter(adapter_vikt);*/
-        
-        Spinner kanslor = (Spinner) findViewById(R.id.spinner_kanslor);
-        ArrayAdapter adapter_kanslor = ArrayAdapter.createFromResource(
-                this, R.array.kanslor, android.R.layout.simple_spinner_item);
-        adapter_kanslor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        kanslor.setAdapter(adapter_kanslor);
-    }
-} 
+		// Open Handler
+		offDrawer.setOnDrawerOpenListener(new OnDrawerOpenListener() {
+			@Override
+			public void onDrawerOpened() {
+				// listView.setVisibility(ListView.GONE);
+			}
+		});
+
+		// Closing Handler
+		offDrawer.setOnDrawerCloseListener(new OnDrawerCloseListener() {
+			@Override
+			public void onDrawerClosed() {
+				// listView.setVisibility(ListView.VISIBLE);
+			}
+		});
+
+		Spinner kanslor = (Spinner) findViewById(R.id.spinner_kanslor);
+		ArrayAdapter adapter_kanslor = ArrayAdapter.createFromResource(this,
+				R.array.kanslor, android.R.layout.simple_spinner_item);
+		adapter_kanslor
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		kanslor.setAdapter(adapter_kanslor);
+	}
+}
