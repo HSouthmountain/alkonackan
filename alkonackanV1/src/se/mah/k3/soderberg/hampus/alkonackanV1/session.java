2be811b58cff3b0c8dce6c36785202375ed7fb3a
Welde <Welde@10.1.1.65>
2011-05-19 14:40:48 +0200

    Signed-off-by: Welde <Welde@10.1.1.65>

--- alkonackanV1/src/se/mah/k3/soderberg/hampus/alkonackanV1/session.java
+++ alkonackanV1/src/se/mah/k3/soderberg/hampus/alkonackanV1/session.java
@@ -52,39 +52,156 @@ public class session extends Activity {
 		adapter_kanslor
 				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		kanslor.setAdapter(adapter_kanslor);
-		
+
 		Button avsluta_session = (Button) findViewById(R.id.buttonAvslutaSession);
 		avsluta_session.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Intent show_overview = new Intent(session.this, overview.class);
 				startActivity(show_overview);
 			}
-		});	
-		
-		//Drickknappar
-		
+		});
+
+		// Drickknappar
+
 		ImageButton button_stark = (ImageButton) findViewById(R.id.button_stark);
 		button_stark.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Spinner kansla = (Spinner) findViewById(R.id.spinner_kanslor);
 				EditText varText = (EditText) findViewById(R.id.editText_var);
 				EditText vemText = (EditText) findViewById(R.id.editText_vem);
 
-				Toast toast = Toast.makeText(getApplicationContext(),
-						"KŠnsla: " + kansla.getSelectedItem() + "\nVart: " + varText.getText() + " \nMed: " + vemText.getText() + " \n\n50cl starkšl",
+				Toast toast = Toast.makeText(
+						getApplicationContext(),
+						"KŠnsla: " + kansla.getSelectedItem() + "\nVart: "
+								+ varText.getText() + " \nMed: "
+								+ vemText.getText() + " \n\nStor stark",
 						Toast.LENGTH_SHORT);
 				toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
 				toast.show();
 
 				varText.clearFocus();
-				vemText.clearFocus();	
+				vemText.clearFocus();
 			}
 		});
-		
-		
+
+		ImageButton button_folk = (ImageButton) findViewById(R.id.button_folk);
+		button_folk.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Spinner kansla = (Spinner) findViewById(R.id.spinner_kanslor);
+				EditText varText = (EditText) findViewById(R.id.editText_var);
+				EditText vemText = (EditText) findViewById(R.id.editText_vem);
+
+				Toast toast = Toast.makeText(
+						getApplicationContext(),
+						"KŠnsla: " + kansla.getSelectedItem() + "\nVart: "
+								+ varText.getText() + " \nMed: "
+								+ vemText.getText() + " \n\nFolkšl",
+						Toast.LENGTH_SHORT);
+				toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+				toast.show();
+
+				varText.clearFocus();
+				vemText.clearFocus();
+			}
+		});
+
+		ImageButton button_drink = (ImageButton) findViewById(R.id.button_drink);
+		button_drink.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Spinner kansla = (Spinner) findViewById(R.id.spinner_kanslor);
+				EditText varText = (EditText) findViewById(R.id.editText_var);
+				EditText vemText = (EditText) findViewById(R.id.editText_vem);
+
+				Toast toast = Toast.makeText(
+						getApplicationContext(),
+						"KŠnsla: " + kansla.getSelectedItem() + "\nVart: "
+								+ varText.getText() + " \nMed: "
+								+ vemText.getText() + " \n\nDrink 4 cl",
+						Toast.LENGTH_SHORT);
+				toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+				toast.show();
+
+				varText.clearFocus();
+				vemText.clearFocus();
+			}
+		});
+
+		ImageButton button_oel = (ImageButton) findViewById(R.id.button_oel);
+		button_oel.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Spinner kansla = (Spinner) findViewById(R.id.spinner_kanslor);
+				EditText varText = (EditText) findViewById(R.id.editText_var);
+				EditText vemText = (EditText) findViewById(R.id.editText_vem);
+
+				Toast toast = Toast.makeText(
+						getApplicationContext(),
+						"KŠnsla: " + kansla.getSelectedItem() + "\nVart: "
+								+ varText.getText() + " \nMed: "
+								+ vemText.getText() + " \n\n33 cl 5,2%",
+						Toast.LENGTH_SHORT);
+				toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+				toast.show();
+
+				varText.clearFocus();
+				vemText.clearFocus();
+			}
+		});
+
+		ImageButton button_vin = (ImageButton) findViewById(R.id.button_vin);
+		button_vin.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Spinner kansla = (Spinner) findViewById(R.id.spinner_kanslor);
+				EditText varText = (EditText) findViewById(R.id.editText_var);
+				EditText vemText = (EditText) findViewById(R.id.editText_vem);
+
+				Toast toast = Toast.makeText(
+						getApplicationContext(),
+						"KŠnsla: " + kansla.getSelectedItem() + "\nVart: "
+								+ varText.getText() + " \nMed: "
+								+ vemText.getText() + " \n\nEtt glas vin",
+						Toast.LENGTH_SHORT);
+				toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+				toast.show();
+
+				varText.clearFocus();
+				vemText.clearFocus();
+			}
+		});
+
+		ImageButton button_shot = (ImageButton) findViewById(R.id.button_shot);
+		button_shot.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Spinner kansla = (Spinner) findViewById(R.id.spinner_kanslor);
+				EditText varText = (EditText) findViewById(R.id.editText_var);
+				EditText vemText = (EditText) findViewById(R.id.editText_vem);
+
+				Toast toast = Toast.makeText(
+						getApplicationContext(),
+						"KŠnsla: " + kansla.getSelectedItem() + "\nVart: "
+								+ varText.getText() + " \nMed: "
+								+ vemText.getText() + " \n\n4 cl Shot",
+						Toast.LENGTH_SHORT);
+				toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+				toast.show();
+
+				varText.clearFocus();
+				vemText.clearFocus();
+			}
+		});
+
 	}
 }
